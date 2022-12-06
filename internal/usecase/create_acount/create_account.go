cdf062fce4e3f2de57001d834190b1c2ceff3edb
Ramon Pacheco <rmpacheco2@gmail.com>
2022-12-05 21:47:55 -0300

    add: acount use case

---
+++ internal/usecase/create_acount/create_account.go
@@ -0,0 +1,42 @@
+package createacount
+
+import (
+	"github.com/ramonmpacheco/ms-wallet/internal/entity"
+	"github.com/ramonmpacheco/ms-wallet/internal/gateway"
+)
+
+type CreateAccountInputDTO struct {
+	ClientId string
+}
+
+type CreateAccountOutputDTO struct {
+	ID string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateway  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(a gateway.AccountGateway, c gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{
+		AccountGateway: a,
+		ClientGateway:  c,
+	}
+}
+
+func (cauc *CreateAccountUseCase) Execute(input CreateAccountInputDTO) (*CreateAccountOutputDTO, error) {
+	client, err := cauc.ClientGateway.Get(input.ClientId)
+	if err != nil {
+		return nil, err
+	}
+
+	account := entity.NewAccount(client)
+	err = cauc.AccountGateway.Save(account)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateAccountOutputDTO{
+		ID: account.ID,
+	}, nil
+}
